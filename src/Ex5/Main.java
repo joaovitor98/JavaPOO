2b6f7fabfa51562a4b9bba3a36af4a3bc48f116a
Joao <vitorj.brandao98@gmail.com>
2025-03-20 00:41:51 -0300

    Construtores, sobrecarga e encapsulamento

--- src/Ex5/Main.java
+++ src/Ex5/Main.java
@@ -9,10 +9,10 @@ public class Main {
         String nomeTitular = leitura.nextLine();
         System.out.print("Digite o número da conta: ");
         int numeroConta = leitura.nextInt();
-        System.out.println("Deseja fazer um deposito inicial?(sim/não)");
-        String dec = leitura.next();
+        System.out.println("Deseja fazer um deposito inicial?(s/n)");
+        char dec = leitura.next().charAt(0);
 
-        if (dec.equalsIgnoreCase("Sim") || dec.equalsIgnoreCase("ss") || dec.equalsIgnoreCase("s")) {
+        if (dec == 's' || dec == 'S') {
             System.out.print("Digite qual saldo quer por na conta: ");
             double saldo = leitura.nextDouble();
             ContaBancaria contaBancaria = new ContaBancaria(numeroConta, nomeTitular, saldo);
@@ -24,22 +24,9 @@ public class Main {
             double valor1 = leitura.nextDouble();
             contaBancaria.sacar(valor1);
         }else {
-            System.out.println("Deseja depositar um valor na conta?");
-            String dec1 = leitura.next();
-            if (dec1.equalsIgnoreCase("Sim") || dec1.equalsIgnoreCase("ss") || dec1.equalsIgnoreCase("s")){
-                double saldo = 0;
-                ContaBancaria contaBancaria = new ContaBancaria(numeroConta, nomeTitular, saldo);
-                System.out.print("Digite o valor que deseja depositar na sua conta: ");
-                double valor = leitura.nextDouble();
-                contaBancaria.depositar(valor);
-                System.out.print("Digite o valor que deseja sacar na sua conta: ");
-                double valor1 = leitura.nextDouble();
-                contaBancaria.sacar(valor1);
-                contaBancaria.exibirInformacoes();
-            }
-/*            double saldo = 0;
+            double saldo = 0;
             ContaBancaria contaBancaria = new ContaBancaria(numeroConta, nomeTitular, saldo);
-            contaBancaria.exibirInformacoes();*/
+            contaBancaria.exibirInformacoes();
         }
     }
 }
